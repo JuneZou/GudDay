e586ff12a347203cc1712737b77ca5668da0f4df
June <upupupgoing@126.com>
2017-10-11 18:08:31 +0800

    天气

--- app/src/main/java/com/june/gudday/ui/homebanner/RefreshenableView.kt
+++ app/src/main/java/com/june/gudday/ui/homebanner/RefreshenableView.kt
@@ -44,6 +44,9 @@ class RefreshenableView : LinearLayout {
     /*判断是否正在进行加载数据*/
     var isRefreshing = false
 
+    /*判断是否可以下拉*/
+    var mCanPull = true
+
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
@@ -61,6 +64,7 @@ class RefreshenableView : LinearLayout {
     override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
         motionX = ev.rawX
         motionY = ev.rawY
+        LogUtils.debug(super.onInterceptTouchEvent(ev).toString())
         when (ev.action) {
             MotionEvent.ACTION_DOWN -> {
                 mLastX = motionX
@@ -68,12 +72,15 @@ class RefreshenableView : LinearLayout {
                 return false
             }
             MotionEvent.ACTION_MOVE -> {
-                if (COMPARE_RATE * Math.abs(motionY - mLastY) > Math.abs(mLastX - motionX)) {
+                if (COMPARE_RATE * Math.abs(motionY - mLastY) > Math.abs(mLastX - motionX) /*&& mCanPull*/) {
+                    LogUtils.debug("move")
                     return true
                 }
                 return false
             }
         }
+
+
         return super.onInterceptTouchEvent(ev)
     }
 
@@ -94,7 +101,6 @@ class RefreshenableView : LinearLayout {
     fun doMovent(distance: Float) {
         mCurrentActDistance = Math.min(mRefreshTargetHeight, translationY + distance)
 
-        LogUtils.debug("distance: $mCurrentActDistance")
         if (mCurrentActDistance >= 0f) {
             mRefreshView.translationY = -mRefreshTargetHeight + mCurrentActDistance
             mRefreshSun.scaleX = 1f - (mRefreshTargetHeight - mCurrentActDistance) / mRefreshTargetHeight
@@ -117,12 +123,12 @@ class RefreshenableView : LinearLayout {
     fun reutunInitState() {
         animate().translationY(0f).setDuration(200).start()
         mRefreshView.animate().translationY(-mRefreshTargetHeight).setDuration(200).start()
+        mCanPull = true
     }
 
     fun startRefreshAnimation() {
         isRefreshing = true
         mRefreshView.animate().translationY(mRefreshingTatgetHeight - mRefreshTargetHeight).setDuration(200).setListener(object : AnimatorListenerAdapter(){
-
             override fun onAnimationEnd(animation: Animator?) {
                 super.onAnimationEnd(animation)
                 mRefreshSun.setImageResource(R.drawable.refresh)
@@ -130,9 +136,12 @@ class RefreshenableView : LinearLayout {
                 animation2.isOneShot = false
                 animation2.start()
             }
-
         }).start()
-        animate().translationY(mRefreshingTatgetHeight).setDuration(200).start()
 
+        val trans = mRefreshTargetHeight - mRefreshingTatgetHeight + 0.37 * mRefreshingTatgetHeight - mRefreshTime.height
+        mRefreshSun.animate().translationY(trans.toFloat()).setDuration(200).start()
+        mRefreshTime.animate().translationY((mRefreshTargetHeight + trans.toFloat())/2f - mRefreshTime.top).setDuration(200).start()
+        animate().translationY(mRefreshingTatgetHeight).setDuration(200).start()
+        mCanPull = false
     }
 }
